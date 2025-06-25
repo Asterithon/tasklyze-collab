072d55e06b93dcac099472924224ddcf6e3b4689
ngurahPutra <ngurahputra2425@gmail.com>
2025-06-25 23:39:18 +0800

    add update adn delete for the task

---
+++ config/aksi_delete_task.php
@@ -0,0 +1,16 @@
+<?php
+include("../inc/connec.php");
+$id_task = $_POST['id_task'];
+$id_project = $_GET['id'];
+
+$sql = "DELETE FROM task WHERE id_task = '$id_task'";
+$result = mysqli_query($conn, $sql);
+
+if ($result) {
+    echo "<script>alert('Task succesfully Deleted');
+        window.location = '../?page=project&&id=$id_project';</script>";
+} else {
+    echo "<script>alert('Task Failed to create');
+        window.location = '../?page=project&&id=$id_project';</script>";
+}
+?>
--- style/template/new_task.php
+++ style/template/new_task.php
@@ -1,38 +1,23 @@
 <div class="modal fade" id="newTask" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
-    <form id="taskForm" class="modal-content" action="config/aksi_new_task.php?id=<?php echo $_GET['id'];?>" method="post" novalidate>
+    <form id="taskForm" class="modal-content" action="config/aksi_new_task.php?id=<?php echo $_GET['id']; ?>"
+      method="post" novalidate>
       <div class="modal-header">
         <h5 class="modal-title" id="newTaskModalLabel">New Task</h5>
       </div>
       <div class="modal-body">
         <div class="mb-4">
           <label for="taskTitle" class="form-label">Title</label>
-          <input
-            type="text"
-            class="form-control"
-            id="taskTitle"
-            name="taskTitle"
-            placeholder="Enter task title"
-            required
-            aria-required="true"
-            aria-describedby="titleHelp"
-            autocomplete="off"
-          />
+          <input type="text" class="form-control" id="taskTitle" name="taskTitle" placeholder="Enter task title"
+            required aria-required="true" aria-describedby="titleHelp" autocomplete="off" />
           <div id="titleHelp" class="form-text">Provide a short title for the task.</div>
         </div>
         <div class="mb-4">
           <label for="taskDescription" class="form-label">Description</label>
-          <textarea
-            class="form-control"
-            id="taskDescription"
-            name="taskDescription"
-            rows="4"
-            placeholder="Enter detailed description"
-            required
-            aria-required="true"
-            aria-describedby="descHelp"
-          ></textarea>
+          <textarea class="form-control" id="taskDescription" name="taskDescription" rows="4"
+            placeholder="Enter detailed description" required aria-required="true"
+            aria-describedby="descHelp"></textarea>
           <div id="descHelp" class="form-text">Add helpful details about the task.</div>
         </div>
       </div>
