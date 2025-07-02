8255d45c4f62723216cb341e4c645c14d8454ffd
Asterithon <binandika06@gmail.com>
2025-07-03 05:22:51 +0800

    update

--- project.php
+++ project.php
@@ -75,8 +75,14 @@ $isAdmin = isset($dataRole['role']) && $dataRole['role'] === 'admin';
         <div class="container-fluid">
             <div class="row">
                 <div class="col-sm-6">
-                    <h1><?= $name_project; ?></h1>
+                    <h1> Project : <?= $name_project; ?></h1>
                 </div>
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="?page=dashboard">Home</a></li>
+                <li class="breadcrumb-item active">Project</li>
+              </ol>
+            </div>
             </div>
         </div>
     </section>
